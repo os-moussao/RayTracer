5f4fb2cf2f9fd269e8afa4285c000fc65790cc99
Oussama Moussaoui <omoussaoui1337@gmail.com>
2022-06-06 22:10:46 +0100

    Update RayTracer.cpp
    - Add Classes: Ray, Color, and Sphere
    - Rendring a 2d image

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -28,6 +28,54 @@ public:
 	void normalize() { *this = unity(); }
 };
 
+class Ray {
+public:
+	Vec o, d; // origin, direction
+	Ray(const Vec &o, const Vec &d): o(o), d(d) { this->d.normalize(); }
+};
+
+class Color {
+public:
+	Vec c;
+	Color(const Vec &c) : c(c) {}
+	Color(double r, double g, double b) : c(r,g,b) {}
+	Color operator * (double opacity) { return Color(c*opacity); }
+	void get(ofstream &file) { file << c.x << " " << c.y << " " << c.z << "\n"; }
+};
+
+class Sphere {
+public:
+	Vec c;
+	double r;
+	Color color;
+	Sphere(const Vec &c, double r, const Color &color) : c(c), r(r), color(color) {}
+	bool inside(Vec point) const { return (point-c)*(point-c) <= sqr(r); };
+};
+
+const int H = 1000;
+const int W = 1000;
+Color white(0xff, 0xff, 0xff);
+Color black(0, 0, 0);
+Color red(0xff, 0, 0);
+Color green(0, 0xff, 0);
+Color blue(0, 0, 0xff);
+
+
+int main() {
+	ofstream ppm("pixel_map.ppm");
+	ppm << "P3\n" << W << "\n" << H << "\n" << "255\n";
+
+	Color AmbientLight = white*0.2;
+	Sphere sph(Vec(0,-80,250), 200.0, red);
+	for (int y = 500; y > -500; y--) {
+		for (int x = -500; x < 500; x++) {
+			Color col = AmbientLight;
+			if (sph.inside(Vec(x, y, sph.c.z))) col = red;
+			col.get(ppm);
+		}
+	}
+}
+
 // Scene:
 
 // Camera : o(0, 0, 0) d(0, 0, 1)
